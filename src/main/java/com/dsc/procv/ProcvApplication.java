bc57169c9a08ebdab5c9fb671d3c7a392482fe41
zekid <zkdsc1996@gmail.com>
2025-11-19 01:20:08 +0300

    first commit

---
+++ src/main/java/com/dsc/procv/ProcvApplication.java
@@ -0,0 +1,13 @@
+package com.dsc.procv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsc/procv/ProcvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.procv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
